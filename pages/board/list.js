c8b878035d18e61d36bacfaf0589688ac595484c
DontLikenaming <edirna21@gmail.com>
2023-03-10 15:46:41 +0900

    MariaDB용 Board 클래스 구현 5 - 본문보기

--- pages/board/list.js
+++ pages/board/list.js
@@ -1,6 +1,7 @@
-import fetch from 'isomorphic-unfetch'
+//import fetch from 'isomorphic-unfetch'
 import {useState} from "react";
 import axios from 'axios';
+import Link from "next/link";
 
 const getStpgns = (cpg, alpg) => {
     let stpgns = [];
@@ -112,7 +113,7 @@ export default function List ({boards}) {
                 {boards.boards.map(bd =>
                     <tr>
                         <td key={bd.BNO}>{bd.bno}</td>
-                        <td>{bd.title}</td>
+                        <td><Link href={`/board/view?bno=${bd.bno}`}>{bd.title}</Link></td>
                         <td>{bd.userid}</td>
                         <td>{bd.regdate}</td>
                         <td>{bd.views}</td>
