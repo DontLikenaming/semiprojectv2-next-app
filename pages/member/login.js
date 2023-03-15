f66e804891aa0c67baa54dd6c640e27e5085966b
DontLikenaming <edirna21@gmail.com>
2023-03-15 11:07:43 +0900

    MariaDB Member 클래스 구현 5 - NextAuth 7 - 로그인 시 페이지 리다이렉션

--- pages/member/login.js
+++ pages/member/login.js
@@ -1,4 +1,3 @@
-import axios from "axios";
 import {useState} from "react";
 import {handleInput} from "../../models/Utils";
 import {getSession, signIn} from "next-auth/client";
@@ -23,11 +22,19 @@ export default function Login () {
         //const data = {userid: userid, passwd: passwd};
 
         // signIn('인증 할 때 활용할 Credentials id',{'인증할 때 사용할 정보'}
-        const res = await signIn('userid-passwd-credentials',{
-            userid, passwd, redirect:true
+        //const res = await signIn('userid-passwd-credentials',{
+        const {error} = await signIn('userid-passwd-credentials',{
+            userid, passwd, redirect:false
         });
 
+        // res는 인증성공 여부를 http 상태코드로 알려줌
+        // 인증 성공하면 200, 실패하면 401
         //console.log('pages/login - ', await res.status);
+
+        //error는 인증 성공하면 null, 실패하면 CredentialsSignin로 알려줌
+        console.log('pages/login - ', await error);
+
+        if(error){location.href='/member/failogin'}else {location.href='/member/myinfo'}
 
     };
 
